0bff1ac1ba0712e6d35fd9aa4380a4892911fa2e
Feather <lisafeather@gmail.com>
2014-06-24 17:46:47 -0600

    double loop not working

--- js/game.js
+++ js/game.js
@@ -13,20 +13,23 @@ var word = {
   checkLetters: function(guessedLetters){
     // an array of guessed letters is passed in
     // intersection finds common letters
+    console.log("in guessed letters: " + guessedLetters.length);
     var blanks = [];
-    var correct_letters = _.intersection(this.secretWord, guessedLetters);
+    console.log("blanks: " + blanks);
+    var correctLetters = _.intersection(this.secretWord, guessedLetters);
     var wrongLetters = [];
+
+    console.log("here's correct_letters: " + correctLetters);
     for(var i = 0; i < guessedLetters.length; i++) {
       for(var n = 0; n < this.secretWord.length; n++) {
         blanks.push("_");
-        if(guessedLetters[i] !== this.secretWord[n]) {
-          wrongLetters.push(guessedLetters[i]);
-        } else {
+        if(guessedLetters[i] === this.secretWord[n]) {
           blanks.push(guessedLetters[i]);
         }
       }
     }
-    return [blanks.slice(1,blanks.length),wrongLetters.slice(0,0)];
+    console.log("wrong letters: " + wrongLetters);
+    return [blanks.slice(1,blanks.length),correctLetters];
   }
 };
 
@@ -42,8 +45,8 @@ var player = {
 
     // use checkLetters to get blanks and see if they won
     var results = word.checkLetters(this.guessedLetters);
-    console.log("here is the blanks: " + results[0]);
-    console.log("here is the wrong letter: " + results[1]);
+    console.log("here is blanks: " + results[0]);
+    console.log("here is the right letter: " + results[1]);
 
     game.updateDisplay(results[0], letter, (this.MAX_GUESSES - this.guessedLetters.length));
 
@@ -92,8 +95,6 @@ var game = {
 };
 
 window.onload = function(){
-  //console.log(word.checkLetters(['m', 'q'])[1]);
-
   // Created a losing scenario
   // console.log(player.checkLose(wrongLettersArray));
 
@@ -106,12 +107,11 @@ window.onload = function(){
   // Add event listener to the letter input field to grab letters that are guessed
   $("#letterField").on("keyup", function() {
     var letter = $("#letterField").val();
+    $("#letterField").val("");
 
     player.makeGuess(letter);
-    $("#letterField").val("");
-    $("#wordString").val("");
-    $("#guessesLeft").val("");
   });
+
   // Add event listener to the reset button to reset the game when clicked
   $("#giveUpButton").on("click", function() {
     console.log("click giveup ");
