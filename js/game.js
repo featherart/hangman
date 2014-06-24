c3a8492113193974b71d45fd9b466b03499f47d4
Feather <lisafeather@gmail.com>
2014-06-24 17:02:15 -0600

    need to implement win/lose scenarios

--- js/game.js
+++ js/game.js
@@ -12,16 +12,21 @@ var word = {
 
   checkLetters: function(guessedLetters){
     // an array of guessed letters is passed in
+    // intersection finds common letters
+    var blanks = [];
     var correct_letters = _.intersection(this.secretWord, guessedLetters);
     var wrongLetters = [];
     for(var i = 0; i < guessedLetters.length; i++) {
       for(var n = 0; n < this.secretWord.length; n++) {
+        blanks.push("_");
         if(guessedLetters[i] !== this.secretWord[n]) {
           wrongLetters.push(guessedLetters[i]);
+        } else {
+          blanks.push(guessedLetters[i]);
         }
       }
     }
-    return ['m____',wrongLetters];
+    return [blanks.slice(1,blanks.length),wrongLetters.slice(0,0)];
   }
 };
 
@@ -31,10 +36,20 @@ var player = {
 
   // Takes a new letter as input and updates the game
   makeGuess: function(letter){
-    var letters = word.secretWord.split("");
+    //var letters = word.secretWord.split("");
     this.guessedLetters.push(letter);
     console.log("in make guess: " + this.guessedLetters);
+
+    // use checkLetters to get blanks and see if they won
+    var results = word.checkLetters(this.guessedLetters);
+    console.log("here is the blanks: " + results[0]);
+    console.log("here is the wrong letter: " + results[1]);
+
+    game.updateDisplay(results[0], letter, (this.MAX_GUESSES - this.guessedLetters.length));
+
     if(this.checkLose(this.guessedLetters) === true) {
+      game.resetGame();
+      game.updateDisplay(this.guessedLetters, word.secretWord, 0);
       console.log("lose game");
     };
 
@@ -67,15 +82,17 @@ var game = {
 
   // Update the display with the parts of the secret word guessed, the letters guessed, and the guesses remaining
   updateDisplay: function(secretWordWithBlanks, guessedLetters, guessesLeft){
-    //$("#wordString").append(secretWordWithBlanks);
-    //$("#guessedLetters").append(guessedLetters);
-    //$("#guessesLeft").append(guessesLeft);
+    $("#wordString").val("");
+    $("#wordString").append(secretWordWithBlanks);
+    $("#guessedLetters").append(guessedLetters);
+    $("#guessesLeft").val("");
+    $("#guessesLeft").append(guessesLeft);
 
   }
 };
 
 window.onload = function(){
-  console.log(word.checkLetters(['m', 'q'])[1]);
+  //console.log(word.checkLetters(['m', 'q'])[1]);
 
   // Created a losing scenario
   // console.log(player.checkLose(wrongLettersArray));
@@ -85,14 +102,15 @@ window.onload = function(){
   // Start a new game
   game.resetGame();
   console.log(word.secretWord);
-  //  console.log("here's letters " + letters);
 
   // Add event listener to the letter input field to grab letters that are guessed
   $("#letterField").on("keyup", function() {
     var letter = $("#letterField").val();
-    $("#guessedLetters").append(letter);
+
     player.makeGuess(letter);
     $("#letterField").val("");
+    $("#wordString").val("");
+    $("#guessesLeft").val("");
   });
   // Add event listener to the reset button to reset the game when clicked
   $("#giveUpButton").on("click", function() {
