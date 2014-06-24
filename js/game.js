58fcfe367e2961bac355272f944e06b4b9d7d5ac
Feather <lisafeather@gmail.com>
2014-06-24 16:16:06 -0600

    partly working

--- js/game.js
+++ js/game.js
@@ -1,11 +1,8 @@
 var word = {
   // This is an attribute of the object word.
-  secretWord: "model",
-  wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'],
+  secretWord: "zeitgeist",
+  wordList: ['ephemeral', 'glow', 'sunset', 'bloom', 'egg', 'rainbow', 'girl', 'diamond', 'verse', 'idiot', 'granny', 'sock', 'tulip', 'jasmine', 'cache', 'dollar', 'quarter', 'pebble', 'bird', 'cowboy', 'panda', 'umbrella', 'blood'],
 
-  // START HERE: Step 1
-  // Selects a random word from the word list sets the secret word
-  // it will set the secretWord attribute from Line 3
   setSecretWord: function(){
     console.log("in setter");
     return this.secretWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
@@ -14,9 +11,7 @@ var word = {
   },
 
   checkLetters: function(guessedLetters){
-    // How can I check against 'model'?
-    // I have an array of guessed letters
-    // I have my secret word of 'model'
+    // an array of guessed letters is passed in
     var correct_letters = _.intersection(this.secretWord, guessedLetters);
     var wrongLetters = [];
     for(var i = 0; i < guessedLetters.length; i++) {
@@ -26,7 +21,7 @@ var word = {
         }
       }
     }
-    return ['m____',wrongLetters]; 
+    return ['m____',wrongLetters];
   }
 };
 
@@ -35,18 +30,27 @@ var player = {
   guessedLetters: [],
 
   // Takes a new letter as input and updates the game
-  makeGuess: function(letter){},
+  makeGuess: function(letter){
+    var letters = word.secretWord.split("");
+    this.guessedLetters.push(letter);
+    console.log("in make guess: " + this.guessedLetters);
+    if(this.checkLose(this.guessedLetters) === true) {
+      console.log("lose game");
+    };
+
+  },
 
   // Check if the player has won and end the game if so
-  checkWin: function(wordString){},
+  checkWin: function(wordString){
+    if(wordString === word.secretWord) {
+      game.resetGame();
+      return true;
+    } else {
+      return false;
+    }
+  },
 
-  // Check if the player has lost and end the game if so
-  // Assume they've guessed [x,z, k, p, q,s,t,v,7]
-  // How can I test this? Where does wrongLetters come from?
   checkLose: function(wrongLetters){
-    // if(!checkWin) {
-    //   wrongLetters;
-    // }
     return wrongLetters.length >= this.MAX_GUESSES;
   }
 };
@@ -54,18 +58,24 @@ var player = {
 var game = {
   // Resets the game
   resetGame: function(){
-  word.setSecretWord();
+    player.guessedLetters = [];
+    word.setSecretWord();
   },
 
   // Reveals the answer to the secret word and ends the game
   giveUp: function(){},
 
   // Update the display with the parts of the secret word guessed, the letters guessed, and the guesses remaining
-  updateDisplay: function(secretWordWithBlanks, guessedLetters, guessesLeft){}
+  updateDisplay: function(secretWordWithBlanks, guessedLetters, guessesLeft){
+    //$("#wordString").append(secretWordWithBlanks);
+    //$("#guessedLetters").append(guessedLetters);
+    //$("#guessesLeft").append(guessesLeft);
+
+  }
 };
 
 window.onload = function(){
-  //console.log(word.checkLetters(['m', 'q'])[1]);
+  console.log(word.checkLetters(['m', 'q'])[1]);
 
   // Created a losing scenario
   // console.log(player.checkLose(wrongLettersArray));
@@ -75,24 +85,23 @@ window.onload = function(){
   // Start a new game
   game.resetGame();
   console.log(word.secretWord);
-  var letters = word.secretWord.split("");
-  console.log("here's letters " + letters);
+  //  console.log("here's letters " + letters);
 
   // Add event listener to the letter input field to grab letters that are guessed
   $("#letterField").on("keyup", function() {
     var letter = $("#letterField").val();
     $("#guessedLetters").append(letter);
-
-    // only append here if letter is correct
-    // check for first letter match
-    if(letter === letters[0] ) {
-      $("#wordString").append(letter);
-    }
+    player.makeGuess(letter);
     $("#letterField").val("");
   });
   // Add event listener to the reset button to reset the game when clicked
-  //$("#giveUpButton").on("click", function() {
-  //  console.log("click ");
-  //)};
+  $("#giveUpButton").on("click", function() {
+    console.log("click giveup ");
+    game.giveUp();
+  });
   // Add event listener to the give up button to give up when clicked
+  $("#resetButton").on("click", function() {
+    console.log("click reset ");
+  });
+
 };
